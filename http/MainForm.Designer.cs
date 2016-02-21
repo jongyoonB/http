60204a232a6f2756f28023a135931b4e857dbbff
jongyoonB <jongyoonb@gmail.com>
2016-02-21 23:17:10 +0900

    httpRequest

---
+++ http/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace http
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button = new System.Windows.Forms.Button();
+            this.reqUrl = new System.Windows.Forms.TextBox();
+            this.source = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size(1123, 544);
+            this.splitContainer1.SplitterDistance = 374;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button);
+            this.splitContainer2.Panel1.Controls.Add(this.reqUrl);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.source);
+            this.splitContainer2.Size = new System.Drawing.Size(374, 544);
+            this.splitContainer2.SplitterDistance = 208;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(102, 85);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(153, 57);
+            this.button.TabIndex = 1;
+            this.button.Text = "button";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // reqUrl
+            // 
+            this.reqUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.reqUrl.Location = new System.Drawing.Point(0, 0);
+            this.reqUrl.Name = "reqUrl";
+            this.reqUrl.Size = new System.Drawing.Size(374, 25);
+            this.reqUrl.TabIndex = 0;
+            this.reqUrl.Text = "naver.com";
+            this.reqUrl.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.reqUrl_KeyPress);
+            // 
+            // source
+            // 
+            this.source.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.source.Location = new System.Drawing.Point(0, 0);
+            this.source.Multiline = true;
+            this.source.Name = "source";
+            this.source.ReadOnly = true;
+            this.source.Size = new System.Drawing.Size(374, 332);
+            this.source.TabIndex = 1;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(745, 544);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1123, 544);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox reqUrl;
+        private System.Windows.Forms.TextBox source;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ http/MainForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace http
+{
+    
+    public partial class MainForm : Form
+    {
+        private System.Uri Url;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void set_Url(String req)
+        {
+            if (req != "")
+            {
+                try
+                {
+                    Url = new System.Uri(req);
+                }
+                catch
+                {
+                    Url = new System.Uri("http://" + req);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter Url");
+            }
+        }
+
+        public void access_Url()
+        {
+            try
+            {
+                webBrowser.Navigate(Url);
+
+            }
+            catch
+            {
+                MessageBox.Show("Check Url");
+            }
+        }
+
+        private void check_loaded(Uri loaded_Url)
+        {
+            if (loaded_Url.Equals(webBrowser.Url))
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(webBrowser.Url);
+                try
+                {
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Stream receiveStream = response.GetResponseStream();
+                        StreamReader readStream = null;
+
+                        if (response.CharacterSet == null)
+                        {
+                            readStream = new StreamReader(receiveStream);
+                        }
+                        else
+                        {
+                            readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                        }
+
+                        string data = readStream.ReadToEnd();
+                        source.Text = data;
+                        response.Close();
+                        readStream.Close();
+                    }
+                    reqUrl.Text = webBrowser.Url.ToString();
+                }
+                catch
+                {
+                    MessageBox.Show("Check Url");
+                    source.Text = "";
+                }
+
+            }
+        }
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            set_Url(reqUrl.Text);
+            access_Url();
+        }
+
+
+        private void reqUrl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                set_Url(reqUrl.Text);
+                access_Url();
+            }
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            check_loaded(e.Url);
+        }
+
+
+    }
+}
